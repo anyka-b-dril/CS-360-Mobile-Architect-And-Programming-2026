950538d982fb7e269f12ee29e9f18ed539647d0f
Anyka B <anyb.drilling@gmail.com>
2026-04-21 18:06:29 -0500

    Add files via upload

---
+++ WeightTrackingApp/ExampleUnitTest.java
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- ALl icons are from: https://icons8.com !-->
+
+<menu xmlns:android="http://schemas.android.com/apk/res/android">
+    <item
+        android:id="@+id/home"
+        android:enabled="true"
+        android:iconTint="@color/black"
+        android:icon="@drawable/home"
+        android:title="@string/homeTitle"/>
+    <item
+        android:id="@+id/record"
+        android:enabled="true"
+        android:iconTint="@color/black"
+        android:icon="@drawable/records"
+        android:title="@string/recordsTitle"/>
+    <item
+        android:id="@+id/you"
+        android:enabled="true"
+        android:iconTint="@color/black"
+        android:icon="@drawable/profile"
+        android:title="@string/profileTitle"/>
+</menu>
